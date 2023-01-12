c570d8c2742f2bf0ab53703e46e7c07f98f069d9
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-12 04:52:16 +0300

    Update comparator.sh

--- comparator.sh
+++ comparator.sh
@@ -3,11 +3,10 @@ set -e
 
 PARSER=./scripts/config
 FILE_1=$1
+FILE_2=$2
 
 for KEY in $(cat ../kernelConfigs/fullKeyList); do
-  echo
-  echo $KEY
-  echo "f: $FILE_1"
-  "$PARSER" --file "$FILE_1" -s $KEY
-  echo
+  RES1=$("$PARSER" --file "$FILE_1" -s $KEY)
+  RES2=$("$PARSER" --file "$FILE_2" -s $KEY)
+  echo -e "$RES1 \t$RES2 \t <--$KEY"
 done 
