7661803e31629890d57c34b2d76d749ec416ea18
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-12 04:47:03 +0300

    Update comparator.sh

--- comparator.sh
+++ comparator.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -e
 
-PARSER=$1
-FILE_1=$2
+PARSER=./scripts/config
+FILE_1=$1
 
 for KEY in $(cat ../kernelConfigs/fullKeyList); do
   echo
   echo $KEY
+  echo "f: $FILE_1"
   "$PARSER" -s $KEY --file "$FILE_1"
   echo
 done 
