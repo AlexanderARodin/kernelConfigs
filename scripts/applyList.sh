ecc5c7edbe8037121cc220c0eccb1a97e7801e0d
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-13 18:54:29 +0300

    Update and rename applyList.sh to scripts/applyList.sh

--- scripts/applyList.sh
+++ scripts/applyList.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -e
 
+ENUM="./kernelConfigs/enum.sh"
+LISTDIR="./kernelConfigs/list"
+
+$ENUM < "$LISTDIR/$1"
+
+exit 0
+
+
+
 echo "\n[PROCESS LIST]"
 while IFS= read -r LINE; do
 	if [ 'x' = "x$LINE" ]; then
