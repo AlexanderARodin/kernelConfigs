72ffccbb3282ba46c94c95b76196b971890600a6
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-13 21:02:17 +0300

    Update applyList.sh

--- scripts/applyList.sh
+++ scripts/applyList.sh
@@ -3,11 +3,11 @@ set -e
 
 cd ./linux-5.15.85
 
-ENUM="../kernelConfigs/scripts/enum-keys.sh"
+ENUM="../kernelConfigs/scripts/enum.sh"
 LISTDIR="../kernelConfigs/list"
 
 $ENUM < "$LISTDIR/$1-applyList" | \
 	while read LINE; do
 		echo "--> apply: $LINE"
-		./scripts/config -s $LINE
+		./scripts/config $LINE
 	done
