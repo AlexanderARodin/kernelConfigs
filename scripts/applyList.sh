784d285d6a6ba12d38552ad31889e1e082e87948
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-13 19:21:05 +0300

    Update applyList.sh

--- scripts/applyList.sh
+++ scripts/applyList.sh
@@ -1,30 +1,13 @@
 #!/bin/sh
 set -e
 
-ENUM="./kernelConfigs/scripts/enum.sh"
-LISTDIR="./kernelConfigs/list"
+cd ./linux-5.15.85
 
-$ENUM < "$LISTDIR/$1" | while read LINE; do
-	echo "--> $LINE"
-done
+ENUM="../kernelConfigs/scripts/enum-keys.sh"
+LISTDIR="../kernelConfigs/list"
 
-exit 0
-
-
-
-echo "\n[PROCESS LIST]"
-while IFS= read -r LINE; do
-	if [ 'x' = "x$LINE" ]; then
-		true
-	else
-		FIRST=$(echo "$LINE" | cut -c 1)
-		if [ '#' = $FIRST ]; then
-			true
-		else
-			if [ '-' = $FIRST ]; then
-				echo "--> apply: $LINE"
-				#./scripts/config $LINE
-			fi
-		fi
-	fi
-done < $1
+$ENUM < "$LISTDIR/$1" | \
+	while read LINE; do
+		echo "--> apply: $LINE"
+		./scripts/config -s $LINE
+	done
