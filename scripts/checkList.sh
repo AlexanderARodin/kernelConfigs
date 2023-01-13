61029043a4827f786d384a1490b257137eb756ba
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-13 20:50:33 +0300

    Update checkList.sh

--- scripts/checkList.sh
+++ scripts/checkList.sh
@@ -3,19 +3,12 @@ set -e
 
 cd ./linux-5.15.85
 
-UNDEF_LIST="E: "
-exec >&9
-
 ENUM="../kernelConfigs/scripts/enum-keys.sh"
 LISTDIR="../kernelConfigs/list"
 
 $ENUM < "$LISTDIR/$1-applyList" | \
 	while read LINE; do
 		RESULT=$( ./scripts/config -s $LINE )
-		if [ 'undef' = $RESULT ]; then
-			echo -n "XXXX" >&9
-			echo "-------"
-		fi
 		echo "$RESULT\t<--$LINE"
 	done
 
